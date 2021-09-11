a61145dc58efb0d179285d82e3f1616c1e965f45
dmandakini <mandakinidas.iiitb@gmail.com>
2021-09-10 23:01:33 -0500

    Create PrintSubArray.java
    PrintSubArrays

---
+++ PrintSubArray/src/PrintSubArray.java
@@ -0,0 +1,21 @@
+
+public class PrintSubArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {10,20,30,40,50};
+		for (int i = 0; i < arr.length; i++) {
+			
+			for (int j = i; j < arr.length; j++) {
+				
+				for (int j2 = i; j2 < j; j2++) {
+					System.out.print(arr[j2]+",");
+				}
+				 System.out.println();
+			}
+			
+		}
+
+	}
+
+}
